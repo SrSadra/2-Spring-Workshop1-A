f513731d52bf83a49a56ccbe1352eb152ec18156
SrSadra <sadraspurs@gmail.com>
2022-02-14 13:02:44 +0330

    calculator

---
+++ ap1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ap1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        String op = input.next();
+        int b = input.nextInt();
+
+        if (op.equals("+")){            
+            System.out.println(a+b);
+        }
+        else if (op.equals("-")){
+            System.out.println(a-b);
+        }
+        else if (op.equals("*")){
+            System.out.println(a*b);
+        }
+        else if (op.equals("/")){
+            System.out.println(a/b);
+        }
+    }
+}
